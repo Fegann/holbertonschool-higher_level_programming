1a96519534fcf95a9afcb49cf95a3feb86b39d0c
Fegann <mustafayevvfagan@gmail.com>
2025-11-20 12:41:42 +0000

    okey

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            my_list[i] = True
+    a = my_list.copy()
+    for i in range(len(a)):
+        if a[i] % 2 == 0:
+            a[i] = True
         else:
-            my_list[i] = False
-    return my_list
+            a[i] = False
+    return a
