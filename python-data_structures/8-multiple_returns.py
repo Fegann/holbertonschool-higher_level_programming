baf1f2fbd51e4ea679b5699e58dd9e463f619cc2
Fegann <mustafayevvfagan@gmail.com>
2025-11-20 12:14:01 +0000

    okey

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = len(sentence), sentence[0] if len(sentence) > 0 else ""
+    a = len(sentence), sentence[0] if len(sentence) > 0 else None
     return a
