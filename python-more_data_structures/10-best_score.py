c7535e9f05745fb4cf055cb46e58fb3668ba2e50
Fegann <mustafayevvfagan@gmail.com>
2025-11-21 07:37:59 +0000

    okey

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if len(a_dictionary) == 0 or a_dictionary == None:
-        print(None)
+        return None
     x = 0
     item = ""
     for i in a_dictionary.keys():
