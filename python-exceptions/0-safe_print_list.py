e600699d1dbbd7f3b6507031f6e51d482f7db619
Fegann <mustafayevvfagan@gmail.com>
2025-11-21 10:23:04 +0000

    okey

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
             a += str(my_list[i])
             b += 1
         a = int(a)
-        return "{}\n{}".format(a,b)
+        return a,b
     except:
         print("error")
