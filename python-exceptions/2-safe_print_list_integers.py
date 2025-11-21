3947d15dd31e21d8f3be6a2857d4b79113026a22
Fegann <mustafayevvfagan@gmail.com>
2025-11-21 10:55:20 +0000

    okey

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end='')
             count += 1
-    except (TypeError, ValueError):
+    except (TypeError, ValueError ,AttributeError):
         pass
     print()
     return count
