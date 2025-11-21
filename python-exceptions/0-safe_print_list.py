67b029c32981bf8fb34fd4008460ae7f467bf999
Fegann <mustafayevvfagan@gmail.com>
2025-11-21 10:15:01 +0000

    okey

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    a = ''
     try:
         for i in range(x):
-            print(str(my_list[i]), end='')
+            a += str(my_list[i])
+        return a
     except:
         print("error")
