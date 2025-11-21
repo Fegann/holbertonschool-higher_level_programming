8f8bb8e9feacc19231b8cb79cff51ae9c0dbbac3
Fegann <mustafayevvfagan@gmail.com>
2025-11-21 06:01:56 +0000

    okey

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     a = matrix.copy()
     for i in range(len(a)):
-        for j in range(len(a[0]):
+        for j in range(len(a[0])):
             a[i][j] **= 2
 return a
