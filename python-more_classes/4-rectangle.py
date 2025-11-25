60dcf3c22e5f74f8ac39595fc0c46a3c7392ab18
Fegann <mustafayevvfagan@gmail.com>
2025-11-25 05:03:10 +0000

    okey

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -50,4 +50,4 @@ class Rectangle:
         return "\n".join(a)
 
     def __repr__(self):
-        return return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
