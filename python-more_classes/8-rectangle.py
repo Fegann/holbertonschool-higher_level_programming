e9a19568979ab7d24f5c5613ffc68da38b74273d
Fegann <mustafayevvfagan@gmail.com>
2025-11-25 07:57:55 +0000

    okey

---
+++ python-more_classes/8-rectangle.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+'''okey'''
+
+
+class Rectangle:
+    '''okey'''
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def __str__(self):
+        symbol = str(self.print_symbol)
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        a = []
+        for i in range(self.__height):
+            a.append(symbol*self.__width)
+        return "\n".join(a)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if area(rect_1) > area(rect_2):
+            return rect_1
+        elif area(rect_2) > area(rect_3):
+            return rect_2
+        else:
+            return rect_1
