0ee99ff9edb5de1a8d7c1721286a03ed0d2d169f
Fegann <mustafayevvfagan@gmail.com>
2025-11-26 13:41:35 +0000

    okey

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 ''' writes text to the file '''
 
+
 def write_file(filename="", text=""):
     ''' okey '''
 
-    with open(filename,"w", encoding="UTF-8") as f:
+    with open(filename, "w", encoding="UTF-8") as f:
         return f.write(text)
