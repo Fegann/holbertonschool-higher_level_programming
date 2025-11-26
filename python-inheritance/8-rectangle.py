812179b718275fa704903b430a42991980f5dd25
Fegann <mustafayevvfagan@gmail.com>
2025-11-26 10:18:09 +0000

    okey git push

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,8 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    ''' okey '''
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
