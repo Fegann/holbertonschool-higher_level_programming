409894bcaaa262c7f2e3fbe0834d71aac4b33033
Fegann <mustafayevvfagan@gmail.com>
2025-11-26 10:34:04 +0000

    okey

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -21,6 +21,8 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
 class Square(Rectangle):
+    ''' okey '''
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
