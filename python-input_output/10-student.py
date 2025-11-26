162eef60535b8c37b4420f31158be3521733d2d8
Fegann <mustafayevvfagan@gmail.com>
2025-11-26 16:06:43 +0000

    okey

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs == None:
+        if attrs is None:
             return self.__dict__
         d = {}
         for key in attrs:
