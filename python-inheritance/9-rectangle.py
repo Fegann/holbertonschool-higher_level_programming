27fff062a3816cf7b09752666a51a62a113af3e8
Fegann <mustafayevvfagan@gmail.com>
2025-11-26 10:27:20 +0000

    okey

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''okey'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' okey '''
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+    def __str__(self, width, height):
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
