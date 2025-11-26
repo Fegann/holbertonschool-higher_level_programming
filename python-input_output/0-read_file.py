6b1522538625e55eed9aadcf5317c4f894554fd0
Fegann <mustafayevvfagan@gmail.com>
2025-11-26 13:28:14 +0000

    okey

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+''' open and read file'''
+
 
 def read_file(filename = ""):
+    ''' open and read file '''
+
     with open("filename", encoding = "UTF-8") as f:
         a = f.read()
         print(a)
