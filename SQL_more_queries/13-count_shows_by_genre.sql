eb69980617acb54cd2b4abdf1dde3cc6b68f6cde
Fegann <mustafayevvfagan@gmail.com>
2025-11-29 19:42:26 +0000

    okey

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- okey
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.genre_id) as number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY number_of_shows DESC;
