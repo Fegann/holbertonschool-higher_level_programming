56d64d500138931adb64a8725d2fdbb1b2b02d41
Fegann <mustafayevvfagan@gmail.com>
2025-11-29 18:28:56 +0000

    okey

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,7 @@
 -- select
 USE hbtn_0d_usa;
 
-SELECT id,name FROM states WHERE name='California';
+SELECT id,name
+FROM cities
+WHERE state_id=(SELECT id FROM states WHERE name='California')
+ORDER BY id;
