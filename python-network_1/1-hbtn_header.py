e94d78315f03637c36338828b185c1e74be47dc6
Fegann <mustafayevvfagan@gmail.com>
2025-12-03 12:19:31 +0000

    okey;

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    a = response.headers
-    print(a['X-Request-Id'])
+
+    with urllib.request.urlopen(url) as response:
+        a = response.headers
+        print(a['X-Request-Id'])
