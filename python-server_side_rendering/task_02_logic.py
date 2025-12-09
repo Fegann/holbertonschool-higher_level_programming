fc3859417b76e3ab0954d07b8b37117841096b54
Fegann <mustafayevvfagan@gmail.com>
2025-12-09 06:34:07 +0000

    okey

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/items')
 def items_page():
-    items = ["Item 1", "Item 2", "Item 3"]
+    items = ["Leonardo", "Raphael", "Donatello", "Michelangelo"]
     return render_template('items.html', items=items)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
